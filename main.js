12177279aa4c21d2a3526f8d98a5ac77a442a58b
Craig Johnson <craigdev937@gmail.com>
2024-07-14 14:31:30 -0400

    Updated the Nav and menu items.

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-console.log("Henry Quartermain!");
+const nav__ul = document.querySelector(".nav__ul");
+const menu__open = document.querySelector(".menu__open");
+const menu__close = document.querySelector(".menu__close");
+const img__background = document.querySelector(".img__background");
+
+menu__open.addEventListener("click", () => {
+    nav__ul.classList.add("active");
+    menu__open.classList.add("active");
+    menu__close.classList.add("active");
+    img__background.classList.add("active");
+});
 
 
 
